b77cc90c0296ad597f93adc684bddcd7c84f0296
Kasey Weirich <knmkonexion@gmail.com>
2020-12-15 15:40:01 -0700

    add kubeconfig

--- scripts/deploy_nginx.sh
+++ scripts/deploy_nginx.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+export KUBECONFIG="$(k3d kubeconfig write k3s-default)"
+
 # Create deployment, service, and ingress
 kubectl apply -f nginx-web/deployment.yaml
 kubectl apply -f nginx-web/service.yaml
