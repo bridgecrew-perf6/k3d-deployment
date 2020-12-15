af9ebb521dd2897f0860f97af3c802baf64dd5d8
Kasey Weirich <knmkonexion@gmail.com>
2020-12-15 14:59:57 -0700

    update args

--- Jenkinsfile
+++ Jenkinsfile
@@ -5,6 +5,8 @@ pipeline {
   parameters {
     string(name: 'CLUSTER_NAME', defaultValue: 'k3s-test', description: 'What do you want to name your k3s cluster?')
     string(name: 'PORT_MAP_LB', defaultValue: '9099:80', description: 'How do you want to map ports on the load balancer <localPort:hostPort>?')
+    string(name: 'API_PORT', defaultValue: '6555', description: 'What API port shall we use for this cluster?')
+    string(name: 'AGENT_NODES', defaultValue: '2', description: 'How many agent nodes do you want in this cluster?')
   }
 
   stages {
@@ -20,7 +22,7 @@ pipeline {
     stage('Create Cluster') {
       steps {
         sh """
-          ./scripts/create_cluster.sh ${CLUSTER_NAME} ${PORT_MAP_LB}
+          ./scripts/create_cluster.sh ${CLUSTER_NAME} ${PORT_MAP_LB} ${API_PORT} ${AGENT_NODES}
         """
       }
     }
--- scripts/create_cluster.sh
+++ scripts/create_cluster.sh
@@ -3,21 +3,24 @@
 clear
 cluster_name=${1}
 port_map_lb=${2}
+api_port=${3}
+agent_nodes=${4}
 
 if [ -z ${cluster_name} ]; then
   echo 'ERROR: cluster name argument not defined, please pass the desired cluster name as an argument.'
   exit 1
-else
-  echo "Creating cluster: ${cluster_name}"
-  echo '------------------------------------------------------'
-fi
-
-if [ -z ${port_map_lb} ]; then
+elif [ -z ${port_map_lb} ]; then
   echo 'ERROR: port mapping argument not defined, please pass the desired port mapping.'
   exit 1
+elif [ -z ${api_port} ]; then
+  echo 'ERROR: api port argument not defined, please pass the desired api port.'
+  exit 1
+  elif [ -z ${agent_nodes} ]; then
+  echo 'ERROR: agent nodes argument not defined, please pass the # of agent nodes to deploy.'
+  exit 1
 else
-  echo "Port mapping to ne used: ${port_map_lb}"
-  echo '------------------------------------------------------'
+  echo "All arguments passed, creating cluter: ${cluster_name}"
+  echo '------------------------------------------------------------------'
 fi
 
 cluster_exists=$(k3d cluster list --no-headers | cut -d' ' -f1)
@@ -28,7 +31,7 @@ if [ "${cluster_exists}" == "${cluster_name}" ]; then
 else
   echo "Creating cluster ${cluster_name}"
   # NOTE this is very specific to deploying a cluster with this port mapping, will need to refactor later....
-  k3d cluster create $CLUSTER_NAME --api-port 6550 -p "${port_map_lb}@loadbalancer" --agents 1
+  k3d cluster create $CLUSTER_NAME --api-port ${api_port} -p "${port_map_lb}@loadbalancer" --agents ${agent_nodes}
   export KUBECONFIG="$(k3d kubeconfig write k3s-default)"
   echo 'Cluster Deployed:'
   k3d cluster list --no-headers | cut -d' ' -f1
