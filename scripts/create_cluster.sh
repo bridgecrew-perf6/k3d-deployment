9c946997eb5f10867dfdd82331fb86ddf1d694aa
Kasey Weirich <knmkonexion@gmail.com>
2020-12-15 14:53:00 -0700

    update port map var

--- scripts/create_cluster.sh
+++ scripts/create_cluster.sh
@@ -28,7 +28,7 @@ if [ "${cluster_exists}" == "${cluster_name}" ]; then
 else
   echo "Creating cluster ${cluster_name}"
   # NOTE this is very specific to deploying a cluster with this port mapping, will need to refactor later....
-  k3d cluster create $CLUSTER_NAME --api-port 6550 -p "9090:80@loadbalancer" --agents 1
+  k3d cluster create $CLUSTER_NAME --api-port 6550 -p "${port_map_lb}@loadbalancer" --agents 1
   export KUBECONFIG="$(k3d kubeconfig write k3s-default)"
   echo 'Cluster Deployed:'
   k3d cluster list --no-headers | cut -d' ' -f1
