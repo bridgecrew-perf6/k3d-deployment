ba46c0c900f495c119ff4227111d24e36c5b9cc2
Kasey Weirich <knmkonexion@gmail.com>
2021-01-09 18:59:56 -0700

    add extra components for image reflector

--- flux/bootstrap_web_app.sh
+++ flux/bootstrap_web_app.sh
@@ -7,6 +7,7 @@ kubectl config use-context k3d-web-application
 flux check --pre
 
 flux bootstrap github \
+  --components-extra=image-reflector-controller,image-automation-controller \
   --owner=$GITHUB_USER \
   --repository=kubernetes \
   --branch=master \
