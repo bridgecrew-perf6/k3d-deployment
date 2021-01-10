64276f8419bae7cbb7b5719a50496d0663830b53
Kasey Weirich <knmkonexion@gmail.com>
2021-01-09 17:00:32 -0700

    add web app cluster

--- scripts/web_app_cluster.sh
+++ scripts/web_app_cluster.sh
@@ -14,22 +14,25 @@ echo '______________________'
 
 echo ''
 echo '---> Creating K3D cluster <---'
-CLUSTER_NAME='black-pearl'
+CLUSTER_NAME='web-application'
 
-# NOTE: the docker.sock volume mapping is needed for Jenkins/Agents to use the Docker daemon
 k3d cluster create $CLUSTER_NAME --api-port 6550 -p '80:80@loadbalancer' -v /mnt/d/k8s_datastore:/cluster_datastore --agents 3 --wait
 
 export KUBECONFIG="$(k3d kubeconfig write ${CLUSTER_NAME})"
 
 echo ''
 echo '---> Creating namespace(s) <---'
-kubectl create -f k8s/namespaces.yaml
+kubectl create namespace dev
+kubectl create namespace stage
+kubectl create namespace prod
 
 echo ''
 echo '---> Create namespace contexts <---'
 kubectl config set-context dev --namespace=dev
 kubectl config set-context stage --namespace=stage
 kubectl config set-context prod --namespace=prod
+
+kubectl get ns
 
 echo ''
 echo '-------------'
